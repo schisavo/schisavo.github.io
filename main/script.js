aeb3a7e011ed9ecdd225370d3c3ce28cd68b2b1c
schisavo <sebastianchisavoforero@gmail.com>
2026-04-21 22:40:20 -0500

    FEAT: Add projects base.

--- main/script.js
+++ main/script.js
@@ -6,27 +6,3 @@ if (window.matchMedia("(hover: hover)").matches) { } else {
     });
   });
 }
-
-/* ·················· Carousel of About technologies ·················· */
-
-const track = document.querySelector(".carousel-track");
-const cards = document.querySelectorAll(".skill-card");
-const prevBtn = document.querySelector(".carousel-btn.prev");
-const nextBtn = document.querySelector(".carousel-btn.next");
-
-let currentIndex = 0;
-
-function updateCarousel() {
-  const offset = -currentIndex * 100;
-  track.style.transform = `translateX(${offset}%)`;
-}
-
-nextBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % cards.length;
-  updateCarousel();
-});
-
-prevBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + cards.length) % cards.length;
-  updateCarousel();
-});
